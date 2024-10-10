32817bc0a4a2c41988fef36fa30a60ae6f00bc24
Divyanshu <dgsingh1937@gmail.com>
2024-10-10 11:11:02 +0530

    Convert LoadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -91,6 +91,24 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productData) => {
+    products = productData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+  });
+  return promise
+}
+
+loadProductsFetch();
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
